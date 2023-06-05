e8790bb57df857ad29de1d9cb34e52896cd7dfc0
yukyung kim <contiki0527@naver.com>
2023-06-05 14:57:32 +0900

    js1

--- src/components/Checklist.jsx
+++ src/components/Checklist.jsx
@@ -1,20 +1,28 @@
-import {React,useState} from "react";
+import { React, useState } from "react";
 import { CiTrash } from "react-icons/ci";
 
-export default function Checkllist(){
-    const [checked, setChecked] = useState(false);
-    const handleChange = () => setChecked((prev) => !prev);
-    return (
-      <div className="list">
-        <input
-          id="checkbox"
-          type="checkbox"
-          value={checked}
-          onChange={handleChange}
-        />
-        <label>공부하기</label>
-        <button><CiTrash/></button>
-      </div>
-    );
-    
+export default function Checkllist() {
+  const [checked, setChecked] = useState(false);
+  const [todo, setTodo] = useState(['거거','아아']);
+  const handleChange = () => setChecked((prev) => !prev);
+  return (
+    <div className="list">
+      <ul>
+        {todo.map((todo) => (
+          <li key={todo.id}>
+            <input
+              id="checkbox"
+              type="checkbox"
+              value={checked}
+              onChange={handleChange}
+            />
+            <p>{todo}</p>
+            <button>
+              <CiTrash />
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { CiLight } from "react-icons/ci";
 import styles from './Navbar.module.css';
 
 export default function Navbar() {
-  return (
+  const [btn1,setBtn1]=useState(0);
+  const [btn2,setBtn2]=useState(0);
+  const [btn3,setBtn3]=useState(0);
+
+  const all=()=>{
+    setBtn1(1);
+    setBtn2(0);
+    setBtn3(0);
+    console.log(btn1,btn2,btn3);
+  };
+  const actives=()=>{
+    setBtn1(0);
+    setBtn2(1);
+    setBtn3(0);
+    console.log(btn1,btn2,btn3);
+
+  };
+  const complete=()=>{
+    setBtn1(0);
+    setBtn2(0);
+    setBtn3(1);
+    console.log(btn1,btn2,btn3);
+
+  };
+  
+  return (    
     <div className={styles.top}>
       <div className={styles.darkmode}>
         <button className={styles.darkmodebtn}>
@@ -11,9 +36,9 @@ export default function Navbar() {
         </button>
       </div>
       <div className={styles.btn}>
-        <button className={styles.btn1}>All</button>
-        <button className={styles.btn2}>Active</button>
-        <button className={styles.btn3}>Complete</button>
+        <button className={btn1===1?`${styles.btn_select}`:`${styles.btn1}`} onClick={all}>All</button>
+        <button className={btn2===1?`${styles.btn_select}`:`${styles.btn2}`} onClick={actives}>Active</button>
+        <button className={btn3===1?`${styles.btn_select}`:`${styles.btn3}`} onClick={complete}>Complete</button>
       </div>
     </div>
   );
